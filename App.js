2c3b2b345743fd4563ce7482265399513539341c
AdenRao <43325549+AdenRao@users.noreply.github.com>
2020-08-10 13:06:11 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  console.log("App is running");
+
+  return (
+    <View style={styles.container}>
+      <Text>Hello, World!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#000000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
